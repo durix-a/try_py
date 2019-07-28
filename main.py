d30744c503aea20fcf953acb3947ec055b9cc8db
Andrey Turin <durix.a@gmail.com>
2019-07-28 22:32:33 +0300

    start in parallel

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import datetime
 from game.best_turn_finder import BestTurnFinder
+from multiprocessing.pool import ThreadPool
+from multiprocessing.pool import Pool
+import os
 
 finder = BestTurnFinder()
-finder.findHighestValueTurnsSequence(12)
+# finder.findHighestValueTurnsSequence(12)
+# print(finder.best_turns_sequence)
+# print(finder.best_turns_sequence_points)
 
-print(finder.best_turns_sequence)
-print(finder.best_turns_sequence_points)
+def findBestTurnsAsync(turn):
+    finder = BestTurnFinder()
+    finder.findTurnsHighestValueSequenceWithPrecalculatedBegginng(9, [turn])
+    print(finder.best_turns_sequence)
+    print(finder.best_turns_sequence_points)
+    return finder.best_turns_sequence_points
 
-print(datetime.datetime.now())
+first_turns = finder.enumerateAllValidTurns()
+# p = ThreadPool(os.cpu_count())
+p = Pool(os.cpu_count())
+print(p.map(findBestTurnsAsync, first_turns))
 
-# a = [1, 3, 4, 5, 7, 8, 0]
 # b = a.copy()
 
 # b.remove(3)
 
 # print(a)
 # print(b)
+
+print(datetime.datetime.now())
